fd9cd0848f97dce71ec7ad629af91403d6a7de46
LubosTran <lubostranpmj@gmail.com>
2019-04-08 21:02:20 +0200

    Double Items, save Matrix for later

---
+++ week-02/day-1/Array and shiet/src/DoubleItems.java
@@ -0,0 +1,12 @@
+// - Create an array variable named `numList`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
+import java.util.*;
+public class DoubleItems {
+    public static void main(String[]args) {
+        int numList[] = {3, 4, 5, 6, 7};
+        for (int a = 0; a < numList.length; a++ ){
+            System.out.println(numList[a]*2);
+        }
+    }
+}
---
+++ week-02/day-1/Array and shiet/src/Matrix.java
@@ -0,0 +1,16 @@
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+import java.util.*;
+public class Matrix {
+    public static void main(String[]args) {
+        int [][] rows = new int [0] [1];
+        System.out.println(Arrays.toString(rows));
+    }
+}
