9eeba2798ebcab82b55921f613a12d490cb4e21e
LubosTran <lubostranpmj@gmail.com>
2019-04-08 16:29:23 +0200

    yay , done this few excercises before

---
+++ week-02/day-2/Array and shiet/src/PrintAll.java
@@ -0,0 +1,10 @@
+// - Create an array variable named `numbers`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `numbers`
+import java.util.*;
+public class PrintAll {
+    public static void main(String[]args){
+        int numbers[] = {4,5,6,7};
+        System.out.println("elements of 'number' is: " + Arrays.toString(numbers));
+    }
+}
