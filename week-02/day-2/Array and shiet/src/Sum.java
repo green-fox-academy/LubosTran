023d9b4df263583288b32bc09e6fb1ae5ce5e204
LubosTran <lubostranpmj@gmail.com>
2019-04-08 14:44:52 +0200

    Took a lot of time to understand this

--- week-02/day-2/Array and shiet/src/Sum.java
+++ week-02/day-2/Array and shiet/src/Sum.java
@@ -1,2 +1,17 @@
+// Create the usual class wrapper and main method on your own.
+
+// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
 public class Sum {
+    public static void main(String [] args) {
+        System.out.println(sum(5));
+    }
+    public static int sum(int numbers) {
+        int sum = 0;
+        for (int i = 0; i <= numbers ; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
 }
+
