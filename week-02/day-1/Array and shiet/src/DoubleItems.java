eb1a4d67147ca079e44bebcadc71e2e0447a4342
LubosTran <lubostranpmj@gmail.com>
2019-04-08 21:08:20 +0200

    Double Items with toString for displaying result

--- week-02/day-1/Array and shiet/src/DoubleItems.java
+++ week-02/day-1/Array and shiet/src/DoubleItems.java
@@ -6,7 +6,8 @@ public class DoubleItems {
     public static void main(String[]args) {
         int numList[] = {3, 4, 5, 6, 7};
         for (int a = 0; a < numList.length; a++ ){
-            System.out.println(numList[a]*2);
+            numList [a] *= 2;
         }
+        System.out.println(Arrays.toString(numList));
     }
 }
