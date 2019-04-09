8df492be284a694eb41f13055c49a0d3bf68b447
LubosTran <lubostranpmj@gmail.com>
2019-04-09 13:53:54 +0200

    DONT OVERTHINK IT, lol 45min for this???

--- week-02/day-2/String&ArraysList/src/UrlFixer.java
+++ week-02/day-2/String&ArraysList/src/UrlFixer.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
+/*
 public class UrlFixer {
+  public static void main(String... args){
+    String url = "https//www.reddit.com/r/nevertellmethebots";
+
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crutial component, find out what it is and insert it too!
+    // Try to solve it more than once using different String functions!
+
+    System.out.println(url);
+  }
+}
+ */
+import java.io.*;
+import java.util.ArrayList;
+
+public class UrlFixer {
+    public static void main(String... args){
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+        //String insertthis = ":";
+        url = url.replace("bots","oods").replace("//", "://");
+
+        System.out.println(url);
+    }
 }
