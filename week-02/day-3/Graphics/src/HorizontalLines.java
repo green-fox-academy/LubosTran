d45b03abe12a4821f84b398748fbe229392dc688
LubosTran <lubostranpmj@gmail.com>
2019-04-10 16:07:51 +0200

    Getting retarded

--- week-02/day-3/Graphics/src/HorizontalLines.java
+++ week-02/day-3/Graphics/src/HorizontalLines.java
@@ -4,43 +4,23 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class ToTheCenter {
+public class HorizontalLines {
     public static void mainDraw(Graphics graphics) {
+        graphics.drawRect(0,0, WIDTH, HEIGHT);
         // Create a line drawing function that takes 3 parameters:
         // The x and y coordinates of the line's starting point and the graphics
-        // and draws a line from that point to the center of the canvas.
+        // and draws a 50 long horizontal line from that point.
         // Draw 3 lines with that function. Use loop for that.
-        graphics.drawRect(0,0,1000,1000);
-        //int three = 2;
-        //for (int row = 0; row < three; row++) {
-        //    for (int column = 0; column < three; column++) {
-        //        if (row == 0
-        //                || row == three -1
-        //                || column == 0
-        //                || column == three -1
-        //                || row == three -1 -column
-        //        ) {
-        //            graphics.drawLine(column *100,row * 100,500,500);
-        //        }
-//
-        //    }
-//
-//
-        //}
-        //drawLineToCenter(graphics, 100,100);
         for (int numberOfLines = 0; numberOfLines<3 ; numberOfLines++) {
-                drawLineToCenter(graphics, numberOfLines*200,numberOfLines*100);
+            HorizontalLines(graphics, 0,(int)(Math.random()*100));
 
         }
 
     }
-    public static void drawLineToCenter(Graphics d, int x, int y) {
-        d.drawLine(x,y,500,500);
-
-
+    public static void HorizontalLines(Graphics d,int x, int y) {
+        d.drawLine(x,y, x+50,y);
 
     }
-
 
     // Don't touch the code below
     static int WIDTH = 1000;
