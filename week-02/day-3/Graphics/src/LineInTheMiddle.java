98a7119fc91502cbebb25444be9de04dedab11bf
LubosTran <lubostranpmj@gmail.com>
2019-04-10 12:32:23 +0200

    line

---
+++ week-02/day-3/Graphics/src/LineInTheMiddle.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LineInTheMiddle {
+    public static void mainDraw(Graphics graphics){
+        // draw a red horizontal line to the canvas' middle.
+        // draw a green vertical line to the canvas' middle.
+
+        graphics.drawRect(0,0, WIDTH,HEIGHT);
+        graphics.setColor(new Color(204, 24, 28));
+        graphics.drawLine(0,500,1000,500);
+        graphics.setColor(new Color(32, 239, 24));
+        graphics.drawLine(500, 0, 500, 1000);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 1000;
+    static int HEIGHT = 1000;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
