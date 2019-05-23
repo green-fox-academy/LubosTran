1403693056f60b66ef58b36af27a0e41c997b248
LubosTran <lubostranpmj@gmail.com>
2019-05-23 10:43:28 +0200

    Login Page - Initialized

--- week-07/Day-4/src/main/java/com/greenfox/pet/pet/controller/MainController.java
+++ week-07/Day-4/src/main/java/com/greenfox/pet/pet/controller/MainController.java
@@ -1,4 +1,15 @@
 package com.greenfox.pet.pet.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
